28a48faeb48354af963e0c11eae94ba8ad0c432e
Kantithat Kanfung <51295343+Kantithat-Kanfung@users.noreply.github.com>
2022-01-31 15:58:43 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,6 +4,15 @@
 
 require "vendor/autoload.php";
 
+/ Get POST body content
+$content = file_get_contents('php://input');
+
+
+
+$events = json_decode($content, true);
+
+if (!is_null($events['events'])){
+
 $access_token = '8LDWtWTIbzxMwRVesUfcU+k23XKJgiz6sf5xgIgkmov/SmGA39BYkqMLOm76tYiXcP1m5P+qMu59LccnmqQIYMjCWa6VmsCYNurdFTLZM7teoqLlNDcNQ7a4j44gLJ1Gb3Lr/ATrPcUA/daju6+cpwdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '585c2145e3e8d1ceae5b3d1e4cca412b';
@@ -17,6 +26,8 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+  
+}
 
 
 
